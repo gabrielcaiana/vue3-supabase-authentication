92f1a6d37d570290b10bf77770caacbfa47cd357
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-30 13:54:40 -0300

    chore: create login ui

--- src/pages/public/AppLogin.vue
+++ src/pages/public/AppLogin.vue
@@ -1,7 +1,102 @@
 <template>
-  <h1>Login</h1>
+  <div class="app-login">
+    <div class="app-login__content">
+      <form class="app-login__form" @submit.prevent="handleLogin">
+        <h1 class="app-login__form__title">Login</h1>
+        <div>
+          <label class="app-login__form__label" for="email">Email</label>
+          <input
+            id="email"
+            v-model="form.email"
+            class="app-login__form__input"
+            type="text"
+            name="email"
+            placeholder="email"
+            required
+          />
+        </div>
+        <div>
+          <label class="app-login__form__label" for="password">Senha</label>
+          <input
+            id="password"
+            v-model="form.password"
+            class="app-login__form__input"
+            type="text"
+            name="password"
+            placeholder="senha"
+            required
+          />
+        </div>
+        <button type="submit" class="app-login__form__button">Entrar</button>
+
+        <router-link class="app-login__form__social-github" to="/forgotPassword"
+          >Esqueceu sua senha?</router-link
+        >
+
+        <!-- <div class="mt-5">
+          <a @click.prevent="handleLogin('github')">Github</a>
+        </div> -->
+      </form>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import useAuthUser from "../../composables/UseAuthUser";
+import { useRouter } from "vue-router";
 
-<style lang="scss" scoped></style>
+const router = useRouter();
+const { login, loginWithSocialProvider } = useAuthUser();
+
+const form = ref({
+  email: "",
+  password: "",
+});
+
+const handleLogin = async (provider) => {
+  try {
+    provider
+      ? await loginWithSocialProvider(provider)
+      : await login(form.value);
+    router.push({ name: "Me" });
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app-login {
+  $this: &;
+  @apply h-screen bg-gray-800 flex justify-center items-center;
+
+  &__content {
+    @apply lg:w-2/5 md:w-1/2 w-2/3;
+
+    #{$this}__form {
+      @apply bg-white p-10 rounded-lg shadow-lg min-w-full;
+
+      &__title {
+        @apply text-center text-2xl mb-6 text-gray-600 font-bold font-sans;
+      }
+
+      &__label {
+        @apply text-gray-800 font-semibold block my-3;
+      }
+
+      &__input {
+        @apply w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none;
+      }
+
+      &__button {
+        @apply w-full mt-6 mb-4 bg-green-700 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans;
+      }
+
+      &__social-github {
+        @apply text-gray-500;
+      }
+    }
+  }
+}
+</style>
